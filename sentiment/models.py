62abc6389f3119bf31ca3678664cfa8b6f8ee9b6
AnuragTheCoder <129210052+AnuragTheCoder@users.noreply.github.com>
2024-06-15 12:31:46 +0530

    changes

--- sentiment/models.py
+++ sentiment/models.py
@@ -1,3 +1,12 @@
+# models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SearchHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    sentiment = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.text[:20]} - {self.sentiment}'
--- sentiment/urls.py
+++ sentiment/urls.py
@@ -1,7 +1,20 @@
+# from django.urls import path
+# from .views import sentiment_view
+
+# urlpatterns = [
+     
+#     path('', sentiment_view, name='sentiment_view'),
+# ]
+# urls.py
 from django.urls import path
-from .views import sentiment_view
+from .views import sentiment_view, LoginPage, SignupPage, LogoutPage, user_history, delete_history,view_history
 
 urlpatterns = [
-     
-    path('', sentiment_view, name='sentiment_view'),
+    path('sentiment/', sentiment_view, name='sentiment_view'),
+    path('login/', LoginPage, name='login'),
+    path('signup/', SignupPage, name='signup'),
+    path('logout/', LogoutPage, name='logout'),
+    # path('history/', user_history, name='user_history'),
+    path('delete/<int:history_id>/', delete_history, name='delete_history'),
+    path('view/<int:history_id>/', view_history, name='view_history'),
 ]
